3e01c34c525a9b505ee24bf86401cef853f1880e
Andrey Gladyshev <Andrey_Gladyshev@epam.com>
2016-11-23 10:59:39 +0600

    Comment to trigger next build.

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -21,7 +21,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * PetClinic Spring Boot Application.
- * 
+ *
  * @author Dave Syer
  *
  */
@@ -29,6 +29,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetClinicApplication {
 
     public static void main(String[] args) throws Exception {
+        //This commet is to trigger next jenkins build
         SpringApplication.run(PetClinicApplication.class, args);
     }
 
